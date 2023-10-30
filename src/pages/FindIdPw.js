e17eab2f42173438719f88f70203c8ccf073eb72
한별 <hanbyeol@hanbyeols-MacBook-Pro.local>
2023-10-30 23:49:40 +0900

    fix 아이디/비밀번호 찾기 제출버튼 추가, 로직 추가

--- src/pages/FindIdPw.js
+++ src/pages/FindIdPw.js
@@ -8,22 +8,40 @@ function FindIdPw(){
     const[name,setName]=useState('');
     const[id,setId]=useState('');
     const[findedId,setFindedId]=useState('');
+    const[findedPw, setFindedPw]=useState('');
 
-    const FindId=()=>{
-        axios.post('http://localhost:8000',{
-            username:name
-        })
-        .then((result)=>{
-            setFindedId()
-        })
-    }
+    async function FindId() {
+        try {
+          const response = await axios.post('http://localhost:8000/findid/',{
+            username:name,
+          });
+          console.log(response);
+          setFindedId(response);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+
+      async function FindPw() {
+        try {
+          const response = await axios.post('http://localhost:8000/findpw/',{
+            usermail:id,
+          });
+          console.log(response);
+          setFindedPw(response);
+        } catch (error) {
+          console.error(error);
+        }
+      }
 
     return(
         <div>
             <h3>아이디찾기</h3>
             <input onChange={(e)=>setName(e.target.value)} placeholder="이름 입력"/>
+            <button onClick={FindId}>확인</button>
             <h3>비밀번호 찾기</h3>
             <input onChange={(e)=>setId(e.target.value)} placeholder="아이디 입력"/>
+            <button onClick={FindPw}>확인</button>
         </div>
     );
 }
